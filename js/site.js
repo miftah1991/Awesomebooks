fd8f8722f8db77b59441b5a01b592d210659d687
miftah1991 <miftah.amin1991@gmail.com>
2022-02-09 19:04:00 +0430

    Add Style, fix linters issue and add strapped row functionality

--- js/site.js
+++ js/site.js
@@ -1,65 +1,64 @@
 let booksList = [];
 
 class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+  constructor(container) {
+    this.container = container;
   }
 
   generateBooks() {
-    container.innerHTML = "";
-    if (localStorage.getItem("books") != null) {
-      booksList = JSON.parse(localStorage.getItem("books"));
+    this.container.innerHTML = '';
+    if (localStorage.getItem('books') != null) {
+      booksList = JSON.parse(localStorage.getItem('books'));
       booksList.forEach((element, index) => {
-        const li = document.createElement("li");
-        const titleDiv = document.createElement("div");
-        titleDiv.className = "book-title";
+        const li = document.createElement('li');
+        li.className = 'elements';
+        const titleDiv = document.createElement('div');
+        titleDiv.className = 'book-title';
         titleDiv.textContent = element.title;
         li.appendChild(titleDiv);
-        const authoorDiv = document.createElement("div");
-        authoorDiv.className = "book-authoor";
+        const authoorDiv = document.createElement('div');
+        authoorDiv.className = 'book-authoor';
         authoorDiv.textContent = element.title;
         li.appendChild(authoorDiv);
-        const removebtn = document.createElement("button");
-        removebtn.className = "remove-button";
-        removebtn.textContent = "Remove";
+        const removebtn = document.createElement('button');
+        removebtn.className = 'remove-button';
+        removebtn.textContent = 'Remove';
 
-        removebtn.addEventListener("click", () => {
-          remove(index);
-          generateBooks();
+        removebtn.addEventListener('click', () => {
+          this.remove(index);
+          this.generateBooks();
         });
         li.appendChild(removebtn);
-        container.append(li);
+        this.container.append(li);
+        if (index % 2 !== 0) {
+          li.classList.add('grey-background');
+        }
       });
     }
   }
 
-  saveDataLocalStorage(booksList) {
-    localStorage.setItem("books", JSON.stringify(booksList));
-  }
+  saveDataLocalStorage = (booksList) => {
+    localStorage.setItem('books', JSON.stringify(booksList));
+  };
 
   remove(index) {
     booksList = booksList.filter((book, ind) => ind !== index);
-    saveDataLocalStorage(booksList);
+    this.saveDataLocalStorage(booksList);
   }
+
   addNewBook() {
-    const title = document.querySelector(".title").value;
-    const author = document.querySelector(".author").value;
+    const title = document.querySelector('.title').value;
+    const author = document.querySelector('.author').value;
     const book = {
       title,
       author,
     };
     booksList.push(book);
-    saveDataLocalStorage(booksList);
-    generateBooks();
+    this.saveDataLocalStorage(booksList);
+    this.generateBooks();
   }
 }
-
-const book = new Book();
-
-const addBookbtn = document.querySelector(".Add");
-const container = document.getElementById("book-lis");
-
-addBookbtn.addEventListener("click", () => addNewBook());
-
-window.onload = generateBooks();
+const book = new Book(document.getElementById('book-lis'));
+const addBookbtn = document.querySelector('.Add');
+addBookbtn.addEventListener('click', () => book.addNewBook());
+window.onload = book.generateBooks();
